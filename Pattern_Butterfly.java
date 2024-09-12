0705181ef1618e1b057a6b684a225d4ae920d8f5
Arvind Choudhary <119117665+arvinddchoudhary@users.noreply.github.com>
2024-09-12 12:18:06 +0530

    Add files via upload

---
+++ Pattern_Butterfly.java
@@ -0,0 +1,38 @@
+public class Pattern_Butterfly {
+
+
+
+
+
+    public static void Butterfly_pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String args []){
+        Butterfly_pattern(4);
+    }
+}
